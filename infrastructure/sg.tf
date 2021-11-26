6c671e6ca1e7438998a2bebd3e24e50e4971e65e
Natan Nascimento <natanascimentom@outlook.com>
2021-11-26 18:51:26 -0300

    feat: added iac code to deploy ec2 machine (#5)

---
+++ infrastructure/sg.tf
@@ -0,0 +1,75 @@
+#Definindo security group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+#Definindo security group
+resource "aws_security_group" "allow_database" {
+  name        = "allow_database"
+  description = "Allow database inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "database from VPC"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_database"
+  }
+}
+#Definindo security group
+resource "aws_security_group" "allow_pgadmin" {
+  name        = "allow_pgadmin"
+  description = "Allow pgadmin inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "pgadmin from VPC"
+    from_port   = 18080
+    to_port     = 18080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_pgadmin"
+  }
+}
