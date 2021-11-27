196aec17b2e36d7450e2a771e6425034bc127d17
Natan Nascimento <natan.nascimento@xpi.com.br>
2021-11-27 09:51:20 -0300

    chore: removing docker-compose from installing scripts

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -6,6 +6,3 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 echo "Docker was installed!"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local /bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-echo "Docker Compose was installed!"
